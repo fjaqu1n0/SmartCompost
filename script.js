00769f8b3d953ce663b8981fe763728cdce19bc9
Francis Jacob Aquino <77041731+fjaqu1n0@users.noreply.github.com>
2023-09-26 13:08:50 +0800

    Update and rename Test.js to script.js
    Added second sensor data functions

--- script.js
+++ script.js
@@ -11,9 +11,14 @@ window.onload = function() {
     // Chart Objects
     var humiChart = $("#HumiChart");
     var airTempChart = $("#AirTempChart");
+    
     var soilTempChart = $("#SoilTempChart");
     var soilMoistureChart = $("#SoilMoistureChart");
     var soilPhChart = $("#SoilpHChart");
+
+     var soilTempChart2 = $("#SoilTempChart2");
+    var soilMoistureChart2 = $("#SoilMoistureChart2");
+    var soilPhChart2 = $("#SoilpHChart2")
 
     //chart instances & configuration
     var commonOptions = {
@@ -98,6 +103,26 @@ window.onload = function() {
         })
     });
 
+    var soilTempChartInstance2 = new Chart(soilTempChart2, {
+        type: 'line',
+        data: {
+          datasets: [{
+              label: "Soil Temperature 2",
+              data: 0,
+              fill: false,
+              borderColor: '#343e9a',
+              borderWidth: 1
+          }]
+        },
+        options: Object.assign({}, commonOptions, {
+          title:{
+            display: true,
+            text: "Soil Temperature ",
+            fontSize: 18
+          }
+        })
+    });
+
     var soilMoistureChartInstance = new Chart(soilMoistureChart, {
         type: 'line',
         data: {
@@ -113,6 +138,26 @@ window.onload = function() {
           title:{
             display: true,
             text: "Soil Moisture",
+            fontSize: 18
+          }
+        })
+    });
+
+    var soilMoistureChartInstance2 = new Chart(soilMoistureChart2, {
+        type: 'line',
+        data: {
+          datasets: [{
+              label: "Soil Moisture 2",
+              data: 0,
+              fill: false,
+              borderColor: '#343e9a',
+              borderWidth: 1
+          }]
+        },
+        options: Object.assign({}, commonOptions, {
+          title:{
+            display: true,
+            text: "Soil Moisture ",
             fontSize: 18
           }
         })
@@ -138,29 +183,78 @@ window.onload = function() {
         })
     });
 
+    var soilPhChartInstance2 = new Chart(soilPhChart2, {
+        type: 'line',
+        data: {
+          datasets: [{
+              label: "Soil pH 2",
+              data: 0,
+              fill: false,
+              borderColor: '#343e9a',
+              borderWidth: 1
+          }]
+        },
+        options: Object.assign({}, commonOptions, {
+          title:{
+            display: true,
+            text: "Soil pH ",
+            fontSize: 18
+          }
+        })
+    });
+
     function addData(data) {
       if(data){
         humiChartInstance.data.labels.push(new Date());
         humiChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(data['airHumidity'])});
+          
         airTempChartInstance.data.labels.push(new Date());
         airTempChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(data['airTemperature'])});
+          
         soilTempChartInstance.data.labels.push(new Date());
         soilTempChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(data['soilTemperature'])});
+
+        soilTempChartInstance2.data.labels.push(new Date());
+        soilTempChartInstance2.data.datasets.forEach((dataset) =>{dataset.data.push(data['soilTemperature2'])});
+          
         soilMoistureChartInstance.data.labels.push(new Date());
         soilMoistureChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(data['soilMoisture'])});
+
+        soilMoistureChartInstance2.data.labels.push(new Date());
+        soilMoistureChartInstance2.data.datasets.forEach((dataset) =>{dataset.data.push(data['soilMoisture2'])});
+
         soilPhChartInstance.data.labels.push(new Date());
         soilPhChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(data['soilpH'])});
+
+        soilPhChartInstance2.data.labels.push(new Date());
+        soilPhChartInstance2.data.datasets.forEach((dataset) =>{dataset.data.push(data['soilpH2'])});
+  
+          
         if(updateCount > numberElements){
           humiChartInstance.data.labels.shift();
           humiChartInstance.data.datasets[0].data.shift();
+            
           airTempChartInstance.data.labels.shift();
           airTempChartInstance.data.datasets[0].data.shift();
+            
           soilTempChartInstance.data.labels.shift();
           soilTempChartInstance.data.datasets[0].data.shift();
+
+          soilTempChartInstance2.data.labels.shift();
+          soilTempChartInstance2.data.datasets[0].data.shift();
+            
           soilMoistureChartInstance.data.labels.shift();
           soilMoistureChartInstance.data.datasets[0].data.shift();
+
+          soilMoistureChartInstance2.data.labels.shift();
+          soilMoistureChartInstance2.data.datasets[0].data.shift();
+            
           soilPhChartInstance.data.labels.shift();
           soilPhChartInstance.data.datasets[0].data.shift();
+
+          soilPhChartInstance2.data.labels.shift();
+          soilPhChartInstance2.data.datasets[0].data.shift();  
+            
         }
         else updateCount++;
         humiChartInstance.update();
@@ -168,6 +262,10 @@ window.onload = function() {
         soilTempChartInstance.update();
         soilMoistureChartInstance.update();
         soilPhChartInstance.update();
+
+        soilTempChartInstance2.update();
+        soilMoistureChartInstance2.update();
+        soilPhChartInstance2.update();
       }
     };
 
