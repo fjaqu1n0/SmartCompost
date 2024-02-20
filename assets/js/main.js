5e183550140e133fdbcda706effad538f0115dd7
Francis Jacob Aquino <77041731+fjaqu1n0@users.noreply.github.com>
2024-02-21 00:09:46 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,4 +64,28 @@
     onscroll(document, toggleBacktotop)
   }
 
+   /**
+     * Initiate Datatables
+     */
+    const datatables = select('.datatable', true)
+    datatables.forEach(datatable => {
+      new simpleDatatables.DataTable(datatable, {
+        perPageSelect: [25, 50, 100, ["All", -1]],
+        columns: [{
+            select: 2,
+            sortSequence: ["desc", "asc"]
+          },
+          {
+            select: 3,
+            sortSequence: ["desc"]
+          },
+          {
+            select: 4,
+            cellClass: "green",
+            headerClass: "red"
+          }
+        ]
+      });
+    })
+
 })();
