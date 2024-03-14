c51dd47cb07073131b549c7ccbf6ec8f5607edbd
Francis Jacob Aquino <77041731+fjaqu1n0@users.noreply.github.com>
2024-03-14 22:08:30 +0800

    Update Controls.php
    - Restructured whole code

--- Controls.php
+++ Controls.php
@@ -29,17 +29,11 @@
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: Nov 17 2023 with Bootstrap v5.3.2
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
@@ -54,6 +48,7 @@
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>        
         </div>
+    
         <nav class="header-nav ms-auto">
             <ul class="d-flex align-items-center">
 
@@ -138,7 +133,7 @@
               <div id="motorButton" class="toggle-btn" data-device="motor"></div>
             </div>
             <br><br>
-            <div class="text3">OFF</div>
+            <div class="text3" id="motorState">OFF</div>
           </div>
           <div class="text3">Speed Control</div>
           <div class="slider-container">
@@ -159,7 +154,7 @@
               <div  id="fanButton" class="toggle-btn" data-device="fan"></div>
             </div>
             <br><br>
-            <div class="text3">OFF</div>
+            <div class="text3" id="fanState">OFF</div>
           </div>
           <div class="text3">Speed Control</div>
           <div class="slider-container">
@@ -179,9 +174,8 @@
             <div class="toggle2">
               <div id="pumpButton" class="toggle-btn" data-device="pump"></div>
             </div>
-            <div class="text2">OFF</div>
             <br><br>
-            <div class="text3">OFF</div>
+            <div class="text3" id="pumpState">OFF</div>
           </div>
           <div class="text3">Speed Control</div>
           <div class="slider-container">
@@ -202,7 +196,7 @@
               <div id="heaterButton" class="toggle-btn" data-device="heater"></div>
             </div>
             <br><br>
-            <div class="text3">OFF</div>
+            <div class="text3" id="heaterState">OFF</div>
           </div>
           <div class="text3">Speed Control</div>
           <div class="slider-container">
@@ -226,19 +220,24 @@
       &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
       Designed by <strong><span>Francis Jacob Aquino</span></strong> using <a href="https://bootstrapmade.com/">BootstrapMade</a>
     </div>
   </footer><!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
   <script src="assets/js/controls.js"></script>
+  
+    <!--  jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 
 </body>
 
