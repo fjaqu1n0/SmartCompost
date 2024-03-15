ed2d8b8725d7b066e91548a8237d3ba0179f2f1b
Francis Jacob Aquino <77041731+fjaqu1n0@users.noreply.github.com>
2024-03-15 22:53:22 +0800

    Update DataTable.php
    - Restructured whole code

--- DataTable.php
+++ DataTable.php
@@ -28,43 +28,78 @@
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link  href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"  rel="stylesheet"/>
-  <link  href="https://cdn.datatables.net/2.0.0/css/dataTables.bootstrap5.css" rel="stylesheet" />
+
+ <!-- Bootstrap 5 Data Table CSS Files -->
+ <link  href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"  rel="stylesheet"/>
+ <link  href="https://cdn.datatables.net/2.0.1/css/dataTables.bootstrap5.css" rel="stylesheet" />
+ <link  href="https://cdn.datatables.net/select/2.0.0/css/select.dataTables.min.css" rel="stylesheet" />
+ 
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-  
-  <!-- Data Table JS -->
+  <link href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" rel="stylesheet" >
+
+  <!-- Boostrap Data Table JS-->
   <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdn.datatables.net/2.0.0/js/dataTables.js"></script>
-  <script src="https://cdn.datatables.net/2.0.0/js/dataTables.bootstrap5.js"></script>
-
-    <!-- Template Main JS File -->
-    <script defer src="assets/js/main.js"></script>
-    <script defer src="assets/js/tables.js"></script>
+  <script src="https://cdn.datatables.net/2.0.1/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.0.1/js/dataTables.bootstrap5.js"></script>
+  <script src="https://cdn.datatables.net/select/2.0.0/js/dataTables.select.min.js"></script>
+     
+  <!-- Template Main JS File -->
   
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: Nov 17 2023 with Bootstrap v5.3.2
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+  <script src="assets/js/tables.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+  
 </head>
 
 <body>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top d-flex align-items-center">
+   <!-- ======= Header ======= -->
+   <header id="header" class="header fixed-top d-flex align-items-center">
 
-    <div class="d-flex align-items-center justify-content-between">  
-        <a href="Home.php" class="logo d-flex align-items-center">
-          <img src="assets/img/mmcm.png" alt="">
-          <span class="d-none d-lg-block">SmartCompost</span>
-        </a>
-        <i class="bi bi-list toggle-sidebar-btn"></i>        
-    </div>
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/mmcm.png" alt="">
+        <span class="d-none d-lg-block">SmartCompost</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
+
+     <nav class="header-nav ms-auto">
+            <ul class="d-flex align-items-center">
+
+                <li class="nav-item dropdown pe-3">
+
+                    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                        <span class="d-none d-md-block dropdown-toggle ps-2">Account</span>
+                    </a><!-- End Profile Iamge Icon -->
+
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center" href="accountManagement.php">
+                                <i class="bi bi-gear"></i>
+                                <span>Account Management</span>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center" href="php/logout.php">
+                                <i class="bi bi-box-arrow-right"></i>
+                                <span>Logout</span>
+                            </a>
+                        </li>
+
+                    </ul><!-- End Profile Dropdown Items -->
+                </li><!-- End Profile Nav -->
+
+            </ul>
+        </nav><!-- End Icons Navigation -->
+
 
   </header><!-- End Header -->
 
@@ -73,33 +108,26 @@
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="Home.php">
-                <i class='bx bx-line-chart'></i>
-                <span>Home</span>
-            </a>
-        </li><!-- End Home Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="Home.php">
+          <i class='bx bx-line-chart'></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Home Nav -->
 
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="Controls.php">
-                <i class='bx bxs-cog'></i>
-                <span>Controls</span>
-            </a>
-        </li><!-- End Controls Page Nav -->
-
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="DataTable.php">
-                <i class='bx bx-table'></i>
-                <span>Data Table</span>
-            </a>
-        </li><!-- End Data Logs Page Nav -->
-
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="php/logout.php"> <!-- Changed href to logout.php -->
-                <i class='bx bx-log-out'></i>
-                <span>Logout</span>
-            </a>
-        </li><!-- End Data Logs Page Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="Controls.php">
+            <i class='bx bxs-cog' ></i>
+          <span>Controls</span>
+        </a>
+      </li><!-- End Controls Page Nav -->
+      
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="DataTable.php">
+          <i class='bx bx-table'></i>
+          <span>Data Table</span>
+        </a>
+      </li><!-- End Data Logs Page Nav -->
 
     </ul>
 
@@ -121,10 +149,12 @@
                 <h5 class="card-title">Sensor Data</h5>
                 <button id="deleteButton">Delete</button>
             </div>
+            
               <!-- Table with stripped rows -->
-              <table id="sensorData" class="table table-striped" style="width:100%">
+              <table id="sensorData" class="table table-striped" style="width:100%"> <!--class="display""table table-striped"-->
                 <thead>
                     <tr>
+                        <th><input type="checkbox" id="selectAll"></th>
                         <th>ID</th>
                         <th>Timestamp</th>
                         <th>Humidity</th>
@@ -145,7 +175,7 @@
         </div>
       </div>
     </section>
-      <div class="button-container">
+    <div class="button-container">
       <div class="left-buttons">
         <button id="EditButton">Edit</button>
         <div class="dropdown">
@@ -165,7 +195,7 @@
           <ul class="dropdown-menu1" aria-labelledby="dropdownMenuButton1">
             <li><a href="#" id="Humidity">Humidity</a></li>
             <li><a href="#" id="AirTemp">Air Temperature</a></li>
-            <li><a href="#" id ="SoilTemp">Soil Moisture</a></li>
+            <li><a href="#" id ="SoilTemp">Soil Temperature</a></li>
             <li><a href="#" id ="Moist">Soil Moisture</a></li>
             <li><a href="#" id ="pH">Soil pH</a></li>
           </ul>
@@ -181,15 +211,13 @@
       &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
       Designed by <strong><span>Francis Jacob Aquino</span></strong> using  <a href="https://bootstrapmade.com/">BootstrapMade</a>
     </div>
   </footer><!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <script src ="assets/js/main.js"></script>
 
 </body>
 
