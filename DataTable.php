fac38a41b1f56b566a385ebd8964ee9ad032548a
Francis Jacob Aquino <77041731+fjaqu1n0@users.noreply.github.com>
2024-03-20 17:58:49 +0800

    Update DataTable.php

--- DataTable.php
+++ DataTable.php
@@ -66,39 +66,30 @@
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
-     <nav class="header-nav ms-auto">
-            <ul class="d-flex align-items-center">
-
-                <li class="nav-item dropdown pe-3">
-
-                    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                        <span class="d-none d-md-block dropdown-toggle ps-2">Account</span>
-                    </a><!-- End Profile Iamge Icon -->
-
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-
-                        <li>
-                            <a class="dropdown-item d-flex align-items-center" href="accountManagement.php">
-                                <i class="bi bi-gear"></i>
-                                <span>Account Management</span>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li>
-                            <a class="dropdown-item d-flex align-items-center" href="php/logout.php">
-                                <i class="bi bi-box-arrow-right"></i>
-                                <span>Logout</span>
-                            </a>
-                        </li>
-
-                    </ul><!-- End Profile Dropdown Items -->
-                </li><!-- End Profile Nav -->
-
-            </ul>
-        </nav><!-- End Icons Navigation -->
+      <nav class="header-nav ms-auto">
+        <ul class="d-flex align-items-center">
+          <li class="nav-item dropdown pe-3">
+              <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                  <span class="d-none d-md-block dropdown-toggle ps-2">Account</span>
+              </a>
+              <ul class="dropdown-menu no-edit dropdown-menu-end dropdown-menu-arrow profile">
+                  <li>
+                      <a class="dropdown-item d-flex align-items-center" href="accountManagement.php">
+                          <i class="bi bi-gear"></i>
+                          <span>Account Management</span>
+                      </a>
+                  </li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li>
+                      <a class="dropdown-item d-flex align-items-center" href="php/logout.php">
+                          <i class="bi bi-box-arrow-right"></i>
+                          <span>Logout</span>
+                      </a>
+                  </li>
+              </ul>
+          </li>
+        </ul>
+      </nav><!-- End Icons Navigation -->
 
 
   </header><!-- End Header -->
@@ -147,9 +138,36 @@
             <div class="card-body">
               <div class="table-header">
                 <h5 class="card-title">Sensor Data</h5>
-                <button id="deleteButton">Delete</button>
-            </div>
-            
+                <button type="button" class="btn btn-dark" id="deleteButton" data-target="#deleteConfirmationModal">
+                    Delete
+                </button>
+                <!-- Modal -->
+                <div class="modal fade" id="deleteConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="deleteConfirmationModalLabel">Confirm Deletion</h5>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        Are you sure you want to delete selected row(s)?
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-primary flex-fill mx-1 col" style="width: 100px;" data-bs-dismiss="modal">Cancel</button>
+                        <!-- <button type="button" class="btn btn-danger" id="confirmDeleteButton">Delete</button> -->
+                        <a href="#" class="btn btn-danger flex-fill mx-1 col" style="width: 100px;" id="confirmDeleteButton">Delete</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Alert when no rows selected -->
+              <!-- <div class="alert alert-warning alert-dismissible fade show" role="alert" id="noRowsAlert" style="display: none;">
+                  No rows selected to delete.
+                  <button type="button" class="btn-close" style="background-color: inherit; color: inherit; border: none;" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </button>
+                </div> -->
+                <div id="alertPlaceholder"></div>
               <!-- Table with stripped rows -->
               <table id="sensorData" class="table table-striped" style="width:100%"> <!--class="display""table table-striped"-->
                 <thead>
@@ -177,15 +195,72 @@
     </section>
     <div class="button-container">
       <div class="left-buttons">
-        <button id="EditButton">Edit</button>
+        <button id="editButton" type="button" class="btn btn-dark" data-bs-target="#editModal">
+          Edit
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editModalLabel">Edit Row</h1>
+              </div>
+              <div class="modal-body">
+                <form id="editForm">
+                  <div class="form-group row mb-3">
+                    <label for="editTimestamp" class="col-sm-3 col-form-label">Timestamp:</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="editTimestamp" name="editTimestamp">
+                    </div>
+                  </div>
+                  <div class="form-group row mb-3">
+                    <label for="editHumidity" class="col-sm-3 col-form-label">Humidity:</label>
+                    <div class="col-sm-9">
+                      <input type="number" class="form-control" id="editHumidity" name="editHumidity">
+                    </div>
+                  </div>
+                  <div class="form-group row mb-3">
+                    <label for="editAirTemp" class="col-sm-3 col-form-label">Air Temperature:</label>
+                    <div class="col-sm-9">
+                      <input type="number" class="form-control" id="editAirTemp" name="editAirTemp">
+                    </div>
+                  </div>
+                  <div class="form-group row mb-3">
+                    <label for="editSoilTemp" class="col-sm-3 col-form-label">Soil Temperature:</label>
+                    <div class="col-sm-9">
+                      <input type="number" class="form-control" id="editSoilTemp" name="editSoilTemp">
+                    </div>
+                  </div>
+                  <div class="form-group row mb-3">
+                    <label for="editSoilMoist" class="col-sm-3 col-form-label">Soil Moisture:</label>
+                    <div class="col-sm-9">
+                      <input type="number" class="form-control" id="editSoilMoist" name="editSoilMoist">
+                    </div>
+                  </div>
+                  <div class="form-group row mb-3">
+                    <label for="editSoilPH" class="col-sm-3 col-form-label">pH:</label>
+                    <div class="col-sm-9">
+                      <input type="number" class="form-control" id="editSoilPH" name="editSoilPH">
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="saveChanges">Update</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End of modal-->
         <div class="dropdown">
           <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" aria-haspopup="true" aria-expanded="false">
             Export
           </button>
           <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <li><a href="#" id="exportCsv">CSV</a></li>
-            <li><a href="#" id="exportSQL">SQL</a></li>
-            <li><a href="#" id ="exportTXT">TXT</a></li>
+            <li><a class="dropdown-item" href="#" id="exportCsv">CSV</a></li>
+            <li><a class="dropdown-item" href="#" id="exportSQL">SQL</a></li>
+            <li><a class="dropdown-item" href="#" id ="exportTXT">TXT</a></li>
           </ul>
         </div>
         <div class="dropdown1">
