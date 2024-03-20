744d0898aabb9f8581fd5488356f41877b23488d
Francis Jacob Aquino <77041731+fjaqu1n0@users.noreply.github.com>
2024-03-20 18:03:26 +0800

    Update tableGenerator.php

--- php/tableGenerator.php
+++ php/tableGenerator.php
@@ -1,30 +1,22 @@
 <?php
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = '';
-$dbname = "testdb";
+include 'db_conn.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 // Include total row count in the response
 $totalRowsSql = "SELECT COUNT(*) AS totalRows FROM sensor_data_single";
-$totalRowsResult = $conn->query($totalRowsSql);
+$totalRowsResult = $con->query($totalRowsSql);
 $totalRowsRow = $totalRowsResult->fetch_assoc();
 
 if (isset($_GET['action']) && $_GET['action'] === 'fetchHighestId') {
     $sql = "SELECT MAX(id) AS highestId FROM sensor_data_single";
-    $result = $conn->query($sql);
+    $result = $con->query($sql);
     $row = $result->fetch_assoc();
     $_SESSION['lastId'] = $row['highestId']; // Store the lastId in the session
     echo json_encode(['highestId' => $row['highestId']]);
 } else {
     $lastId = isset($_SESSION['lastId']) ? $_SESSION['lastId'] : 0;
     $sql = "SELECT * FROM sensor_data_single WHERE id > $lastId ORDER BY id ASC";
-    $result = $conn->query($sql);
+    $result = $con->query($sql);
     $data = array();
     while ($row = $result->fetch_assoc()) {
         $data[] = $row;
@@ -32,5 +24,5 @@ if (isset($_GET['action']) && $_GET['action'] === 'fetchHighestId') {
     echo json_encode($data);
 }
 
-$conn->close();
+$con->close();
 ?>
